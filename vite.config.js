dc29ef629b63a0dddb4ee46accabce5d93dee49e
tiwariParth <243mjparth@gmail.com>
2023-12-15 20:36:33 +0530

    added .env config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    // Pass environment variables to the client-side code
+    "process.env.REACT_APP_API_KEY": JSON.stringify(
+      process.env.REACT_APP_API_KEY
+    ),
+    "process.env.REACT_APP_API_URL": JSON.stringify(
+      process.env.REACT_APP_API_URL
+    ),
+  },
+});
